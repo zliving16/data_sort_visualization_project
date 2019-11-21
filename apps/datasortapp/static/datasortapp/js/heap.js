c87a1665f9e90bef91a84fb3841bea096e21506b
Erik Praitis <Erik_p@outlook.com>
2019-11-21 15:55:47 -0800

    got heap to work

---
+++ apps/datasortapp/static/datasortapp/js/heap.js
@@ -0,0 +1,87 @@
+// QuickSort Visualization
+// Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/143-quicksort.html
+// https://editor.p5js.org/codingtrain/sketches/vic6Qzo-j
+// https://youtu.be/eqo2LxRADhU
+
+let values = [];
+let w = 1;
+
+let states = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  values = new Array(floor(width / w));
+  for (let i = 0; i < values.length; i++) {
+    values[i] = random(height);
+    states[i] = -1;
+  }
+  heapSort(values);
+}
+
+async function heap_root(input, i) {
+  var left = 2 * i + 1;
+  var right = 2 * i + 2;
+  var max = i;
+
+  if (left < array_length && input[left] > input[max]) {
+      max = left;
+  }
+
+  if (right < array_length && input[right] > input[max])     {
+      max = right;
+  }
+
+  if (max != i) {
+      swap(input, i, max);
+      heap_root(input, max);
+  }
+}
+
+async function swap(input, index_A, index_B) {
+  var temp = input[index_A];
+
+  input[index_A] = input[index_B];
+  input[index_B] = temp;
+}
+
+async function heapSort(input) {
+  
+  array_length = input.length;
+
+  for (var i = Math.floor(array_length / 2); i >= 0; i -= 1)      {
+      await sleep(50)
+      heap_root(input, i);
+    }
+
+  for (i = input.length - 1; i > 0; i--) {
+      await sleep(50)
+      swap(input, 0, i);
+      array_length--;
+    
+    
+      heap_root(input, 0);
+  }
+}
+
+function draw() {
+  background(0);
+
+  for (let i = 0; i < values.length; i++) {
+    noStroke();
+    if (states[i] == 0) {
+      fill('#E0777D');
+    } else if (states[i] == 1) {
+      fill('#D6FFB7');
+    } else {
+      fill(255);
+    }
+    rect(i * w, height - values[i], w, values[i]);
+  }
+}
+
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
